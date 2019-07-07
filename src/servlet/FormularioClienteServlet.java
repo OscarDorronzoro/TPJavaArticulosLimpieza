61aa5c0b664b66dd701eaa4100b9d35680bd69d0
Oscar <Oscar@OscarDorronzoro>
2019-07-07 16:11:42 -0300

    correccion

--- src/servlet/FormularioClienteServlet.java
+++ src/servlet/FormularioClienteServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FormularioClienteServlet
  */
-@WebServlet("/FormularioServlet")
+@WebServlet("/FormularioClienteServlet")
 public class FormularioClienteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
