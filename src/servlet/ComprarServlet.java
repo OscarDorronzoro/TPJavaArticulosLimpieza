28884134c275841bfa869cf6faf3978d6850a0c5
IPP <IPP@10.0.2.15>
2019-07-30 14:29:46 -0300

    compra servlet

--- src/servlet/ComprarServlet.java
+++ src/servlet/ComprarServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import logic.ABMCArticulo;
+import entities.Articulo;
+
 
 /**
  * Servlet implementation class ComprarServlet
@@ -27,7 +30,10 @@ public class ComprarServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		ABMCArticulo articuloLogic= new ABMCArticulo();
+		Articulo articulo=articuloLogic.getOne(codArticulo);
+		
 	}
 
 	/**
