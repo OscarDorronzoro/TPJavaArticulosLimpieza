055e97ac0615b89f68604db1b3fde59a1eced1c9
Oscar vcxvcxvvc <dorronzorooskioscar@hotmail.com>
2019-11-09 12:14:06 -0300

    metodo para verificar campos nulos

--- src/util/Validator.java
+++ src/util/Validator.java
@@ -2,7 +2,16 @@ package util;
 
 public class Validator {
 
-	public static boolean isNullField() {
-		return true;
+	public static boolean isNullField(String[] campos) {
+		
+		boolean isNullField=false;
+		int i=0;
+
+		while(i<campos.length && !isNullField) {	
+			if(campos[i]==null || campos[i].equals("")) {
+				isNullField=true;
+			}
+		}
+		return isNullField;
 	}
 }
