693410e814622b12ebf73c9f19b7f0822ae8b8ce
agus- <agus-@DESKTOP-FH8A0KF>
2019-12-09 18:57:40 -0300

    registro de pago empezado

--- src/servlet/RegistrarPagoServlet.java
+++ src/servlet/RegistrarPagoServlet.java
@@ -7,6 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import entities.Cliente;
+import entities.Venta;
+import logic.ABMCCliente;
+import logic.ABMCVenta;
+import util.DoniaMaryException;
+
 /**
  * Servlet implementation class RegistrarPagoServlet
  */
@@ -27,13 +33,30 @@ public class RegistrarPagoServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		switch(request.getPathInfo()) {
+		ABMCVenta abmcVenta = new ABMCVenta();		
 		
+		switch(request.getPathInfo()) {
 		case "/buscar":
-			request.getRequestDispatcher("/WEB-INF/registrarPago.jsp").forward(request, response);
+			try {
+				request.setAttribute("ventas", abmcVenta.getAllPendientesByCliente(request.getParameter("username")));
+				request.getRequestDispatcher("/WEB-INF/registrarPago.jsp").forward(request, response);
+			} catch (DoniaMaryException e) {
+				// TODO Auto-generated catch block
+				response.sendRedirect("errorPage.jsp?mensaje="+e.getMessage());
+			}
 			break;
 		case "/registrar":
 			//buscar venta, asignarle fecha de pago, guardarla
+			try {
+				Venta venta = abmcVenta.getOne(Integer.parseInt(request.getParameter("nroVenta")));
+				request.getRequestDispatcher("WEB-INF/registrarPago.jsp");
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				response.sendRedirect("../errorPage.jsp?mensaje=Numero de venta incorrecto");
+			} catch (DoniaMaryException e) {
+				// TODO Auto-generated catch block
+				response.sendRedirect("../errorPage.jsp?mensaje="+e.getMessage());
+			}
 			break;
 		default: throw new ServletException("error en switch");
 				
