83b1a493e9013148c4c1a8036939316eaa2e7298
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2025-03-12 22:32:45 -0300

    fixed NullPointerException when there is no inner exception

--- src/util/DoniaMaryException.java
+++ src/util/DoniaMaryException.java
@@ -9,9 +9,6 @@ import org.apache.logging.log4j.Logger;
 
 public class DoniaMaryException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public DoniaMaryException(String message) {
@@ -26,19 +23,22 @@ public class DoniaMaryException extends Exception {
 		Logger logger = LogManager.getLogger(getClass());
 		
 		StringWriter sw = new StringWriter();
-		innerException.printStackTrace(new PrintWriter(sw));
-		String stackTrace = sw.toString();
+		String stackTrace = "";
+		if (innerException != null) {
+			innerException.printStackTrace(new PrintWriter(sw));
+			stackTrace = sw.toString();
+		}
 		
-		String mensaje=message;
+		String mensaje = message;
 		Throwable causa = innerException;
 		
-		logger.log(errorLevel,mensaje +"\n\t"+stackTrace);
-		while(causa.getCause()!=null) {
-			causa=causa.getCause();
-			mensaje=causa.getMessage();
+		logger.log(errorLevel, mensaje + "\n\t" + stackTrace);
+		while(causa != null && causa.getCause() != null) {
+			causa = causa.getCause();
+			mensaje = causa.getMessage();
 			causa.printStackTrace(new PrintWriter(sw));
 			stackTrace = sw.toString();
-			logger.log(errorLevel,mensaje +"\n\t"+stackTrace);			
+			logger.log(errorLevel,mensaje + "\n\t" + stackTrace);			
 		}
 		
 	}
